420fb432d4e07fab2ee4ec9caab0a79c43a31bab
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-04-27 14:13:20 +0200

    Working version of API for GET and POST.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,95 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Router } from "itty-router";
+import { createClient } from "@supabase/supabase-js";
+import { z } from "zod";
+import { Database } from "./types/supabase";
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+  SUPABASE_URL: string;
+  SUPABASE_ANON_KEY: string;
 }
 
+const temperatureSchema = z.object({
+  location: z.number(),
+  time: z.number(),
+  temperature: z.number(),
+});
+
+const router = Router();
+
+router.get("/api/cities", async (request, { SUPABASE_URL, SUPABASE_ANON_KEY }: Env) => {
+  const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+  const { data } = await supabase.from("city").select("*");
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+});
+
+router.get("/api/cities/:id", async (request, { SUPABASE_URL, SUPABASE_ANON_KEY }: Env) => {
+  const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+  const { data } = await supabase.from("city").select("*").eq("id", request.params.id);
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+});
+
+router.get("/api/temperatures", async (request, { SUPABASE_URL, SUPABASE_ANON_KEY }: Env) => {
+  const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+  const { data } = await supabase.from("temperature").select("*");
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+});
+
+router.post("/api/temperatures", async (request, { SUPABASE_URL, SUPABASE_ANON_KEY }: Env) => {
+  const body = await request.json();
+
+  const parsedBody = temperatureSchema.safeParse(body);
+
+  if (!parsedBody.success) {
+    return new Response(JSON.stringify(parsedBody.error), {
+      status: 404,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+  const { data, error } = await supabase.from("temperature").insert(parsedBody.data);
+
+  if (error) {
+    return new Response(JSON.stringify(error.message), {
+      status: 404,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+});
+
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  fetch: router.handle,
 };
