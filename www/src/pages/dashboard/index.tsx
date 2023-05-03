959bcf60c0e40d2639fb6f3fbaaa49f4bf3d337a
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-05-03 15:56:46 +0200

    Implemented throttling. Updates for responsiveness. Update for API.

--- www/src/pages/dashboard/index.tsx
+++ www/src/pages/dashboard/index.tsx
@@ -50,7 +50,7 @@ export default function Dashboard() {
   const outlet = useOutlet();
 
   return (
-    <div className="min-h-screen relative overflow-y-auto flex gap-6 p-6">
+    <div className="min-h-screen relative overflow-y-auto flex gap-6 p-4 md:p-6">
       <aside className="hidden top-0 bottom-0 shrink-0 w-24 bg-gray-800 rounded-lg sticky md:flex flex-col items-center gap-8 py-6">
         <Link className="flex flex-col items-center gap-1" to="/dashboard">
           <img src="/logo.svg" alt="Logo" className="w-10" />
